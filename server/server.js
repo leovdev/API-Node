fd2fbfdcc24740cecadd559a244092d33eeec131
leo <leonelvinasco@gmail.com>
2019-05-27 18:45:25 -0500

    Validation, required fields, default value in fields of the object , trim, min length

--- server/server.js
+++ server/server.js
@@ -5,24 +5,38 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 //save new Something
 var Todo = mongoose.model('Todo', {
   text: {
-    type:String
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default:false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default:  null
   }
 });
 
-var newTodo = new Todo({
-  text: 'Cook dinner',
-  completed: false,
-  completedAt: 201905271513
-});
+// var newTodo = new Todo({
+//   text: 'Cook dinner',
+//   completed: false,
+//   completedAt: 201905271513
+// });
+//
+// newTodo.save().then((doc)=>{
+//   console.log('Saved todo', doc);
+// }, (e)=>{
+//   console.log('Unable to save todo');
+// });
 
-newTodo.save().then((doc)=>{
-  console.log('Saved todo', doc);
+var otherTodo = new Todo ({
+ text: ' true '
+});
+otherTodo.save().then((doc)=>{
+  console.log(JSON.stringify(doc,undefined,2));
 }, (e)=>{
-  console.log('Unable to save todo');
+  console.log('unable to save', e);
 });
