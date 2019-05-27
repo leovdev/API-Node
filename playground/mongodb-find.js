537e586c916ce42f502a47f6897a45a32f0de122
leo <leonelvinasco@gmail.com>
2019-05-27 12:05:33 -0500

    Filtering results with find() to obtain not completed todos or pending tasks

---
+++ playground/mongodb-find.js
@@ -0,0 +1,24 @@
+//const MongoClient= require('mongodb').MongoClient;
+const {MongoClient, ObjectID}= require('mongodb');
+
+var obj=new ObjectID();
+console.log(obj);
+
+//se crea el cliente de mongo, se conecta al servidor
+//mongo y se crea la bd
+MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true }, (err,client)=>{
+  if (err){
+    return console.log('Unable to connect to MongoDB server');
+
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+  db.collection('Todos').find({completed:false}).toArray().then( (docs)=>{
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined,2));
+  }, (err)=>{
+    console.log('Error: Unable to fetch Todos', err);
+  });
+
+  //client.close();
+});
