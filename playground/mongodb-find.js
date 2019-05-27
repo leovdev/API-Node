66056fd612a5a91fd045b6fae4b8a0c6bc648428
leo <leonelvinasco@gmail.com>
2019-05-27 12:29:33 -0500

    Using cursor library, count method to count the collection documents

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -13,11 +13,8 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true
   }
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
-  db.collection('Todos').find({
-    _id: new ObjectID('5ce367fd7b900734c9a45d3a')
-  }).toArray().then( (docs)=>{
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined,2));
+  db.collection('Todos').find().count().then( (count)=>{
+    console.log(`Todos count: ${count}`);
   }, (err)=>{
     console.log('Error: Unable to fetch Todos', err);
   });
