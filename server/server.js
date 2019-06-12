d636d9c29fdd20ea133a6cea6c7fa6771df582fb
leo <leonelvinasco@gmail.com>
2019-06-12 08:15:22 -0500

    POST funcionando, prueba con POSTMan. Ejecutar bd con -path ...

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var app= express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req,res)=>{
-    // console.log(req.body);
+     console.log(req.body);
     var todo = new Todo({
       text: req.body.text
     });
@@ -28,3 +28,5 @@ app.post('/todos', (req,res)=>{
 app.listen(3000, ()=>{
   console.log('Started on port 3000');
 });
+
+// module.exports = {app};
---
+++ server/tests/server.test.js
@@ -0,0 +1,37 @@
+const expect = require('expect');
+const request = require('supertest');
+
+var {Todo}= require('./../models/todo');
+//When doing destructuring is necesary use var?
+const {app} =require('./../server');
+//const {Todo} require('./server/models/todo.js');
+
+beforeEach((done) =>{ //With this method our database ir going to be empty before every request
+  Todo.remove({}).then(()=>done());
+});
+
+
+describe('POST /todos', ()=>{
+  it('should create a new todo', (done)=>{
+    var text= 'text todo text';
+
+    request(app)
+       .post('/todos')
+       .send({text})
+       .expect(200)
+       .expect((res) =>{
+          expect(res.body.text).toBe(text);
+       })
+       .end((err,res)=>{
+         if (err) {
+           return done(err);
+         }
+
+         Todo.find().then((todos)=>{
+           expect(todos.length).toBe(1);
+           expect(todos[0].text).toBe(text);
+           done();
+         }).catch((e)=> done(e));
+       });
+  });
+});
